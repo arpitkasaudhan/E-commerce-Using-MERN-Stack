771f8014259b3ac642e8b72ddf78efb403cf94fd
arpitkasaudhan <kasaudhanarpit37@gmail.com>
2024-03-14 20:59:46 +0530

    Order success and my orders

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,11 @@
 import Cart from '../features/cart/Cart'
+import NavBar from '../features/navbar/Navbar';
 function CartPage() {
   return (
+    <NavBar>
     <Cart></Cart>
+    </NavBar>
+   
   )
 }
 
